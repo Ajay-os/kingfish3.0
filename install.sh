481553eaf308600d00cc3ed127cb108b40dce5d9
Ajay o s <akhubsmaster@gmail.com>
2022-01-26 16:41:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ apt upgrade
 pkg install python python2 git curl php -y
 git clone https://github.com/Ajay-o-s/kingfish3.0.git
 cd kingfish3.0
+pkg install php -y
 chmod +x social.py
 python3 social.py
